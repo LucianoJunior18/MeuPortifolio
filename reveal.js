ec9221c1e9da1db4cc9c1d6a87b11e0782eff218
junior <junioristylo@gmail.com>
2025-02-06 17:32:39 -0300

    primeiro commit da criacao do meu portifolio

---
+++ reveal.js
@@ -0,0 +1,21 @@
+let navBar = document.querySelector("#header");
+
+document.addEventListener("scroll", () => {
+  let scrollTop = window.scrollY;
+
+  if (scrollTop > 0) {
+    navBar.classList.add("rolar");
+  } else {
+    navBar.classList.remove("rolar");
+  }
+});
+
+// zera o formulario do email apois o envio
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    // Aguarda o envio do formulário e limpa os campos
+    setTimeout(() => {
+      this.reset();
+    }, 100); // Pequeno atraso para garantir que o envio seja concluído
+  });
