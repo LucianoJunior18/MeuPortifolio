1c13b58eca0af74c3ed3cb7e76d6b80c4b1341a5
junior <junioristylo@gmail.com>
2025-02-12 15:39:34 -0300

    atualisando o menu mobile e melhorando a responsividade e layout do site

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ let navBar = document.querySelector("#header");
 let menuIcon = document.querySelector("#menu-icon");
 let navBarMenu = document.querySelector("header nav");
 
+// Abrir ou fechar o menu ao clicar no ícone do menu
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("bx-x");
   navBarMenu.classList.toggle("active");
 });
 
+// Fechar o menu ao clicar em qualquer link
+const menuLinks = document.querySelectorAll("header nav a");
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    // Fecha o menu quando um link for clicado
+    menuIcon.classList.remove("bx-x");
+    navBarMenu.classList.remove("active");
+  });
+});
+
+// Adicionando um efeito de rolar no cabeçalho ao rolar a página
 document.addEventListener("scroll", () => {
   let scrollTop = window.scrollY;
-
   if (scrollTop > 0) {
     navBar.classList.add("rolar");
   } else {
@@ -17,12 +28,11 @@ document.addEventListener("scroll", () => {
   }
 });
 
-// zera o formulario do email apois o envio
+// Zera o formulário de e-mail após o envio
 document
   .getElementById("contactForm")
   .addEventListener("submit", function (event) {
-    // Aguarda o envio do formulário e limpa os campos
     setTimeout(() => {
       this.reset();
-    }, 100); // Pequeno atraso para garantir que o envio seja concluído
+    }, 100); // Atraso para garantir que o envio seja concluído
   });
